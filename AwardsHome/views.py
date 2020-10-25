153a5244b98402bec2621207bbc19c77da88d745
Irene-nandy <irenekasiva75@gmail.com>
2020-10-25 19:10:12 +0300

    add views AwardsHome

--- AwardsHome/views.py
+++ AwardsHome/views.py
@@ -1,50 +1,50 @@
-# from django.shortcuts import render, redirect
-# from AwardsPosts . models import Post, Ratings
-# from AwardsPosts.forms import RatingForm
-# from django.views.decorators.csrf import csrf_protect
-# from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from AwardsPosts . models import Post, Ratings
+from AwardsPosts.forms import RatingForm
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.decorators import login_required
 
 
-# # Create your views here.
+# Create your views here.
 
-# @csrf_protect
-# def home(request):
-#     post = Post.objects.all().order_by('-last_modified')
+@csrf_protect
+def home(request):
+    post = Post.objects.all().order_by('-last_modified')
 
-#     context={
-#         'posts' : post,
-#     }
-#     return render(request, 'AwardsHome/home.html', context)
+    context={
+        'posts' : post,
+    }
+    return render(request, 'AwardsHome/home.html', context)
 
-# def about(request):
-#     return render(request, 'AwardsHome/about.html', {'title':'About'})
+def about(request):
+    return render(request, 'AwardsHome/about.html', {'title':'About'})
 
-# @login_required
-# @csrf_protect
-# def rating(request, pk):
-#     post = Post.objects.get(pk=pk)
-#     user = request.user
+@login_required
+@csrf_protect
+def rating(request, pk):
+    post = Post.objects.get(pk=pk)
+    user = request.user
 
-#     if request.method == 'POST':
-#         form = RatingForm(request.POST)
-#         if form.is_valid():
-#             rating = Ratings(
-#                 author = user,
-#                 post = post,
-#                 design_rating = form.cleaned_data["design_rating"],
-#                 usability_rating = form.cleaned_data["usability_rating"],
-#                 content_rating = form.cleaned_data["content_rating"],
-#                 comment = form.cleaned_data["comment"],
-#             )
-#             rating.save()
+    if request.method == 'POST':
+        form = RatingForm(request.POST)
+        if form.is_valid():
+            rating = Ratings(
+                author = user,
+                post = post,
+                design_rating = form.cleaned_data["design_rating"],
+                usability_rating = form.cleaned_data["usability_rating"],
+                content_rating = form.cleaned_data["content_rating"],
+                comment = form.cleaned_data["comment"],
+            )
+            rating.save()
 
-#     else:
-#         form = RatingForm()
+    else:
+        form = RatingForm()
 
-#     ratings = Ratings.objects.filter(post=post).order_by('-created_on')
-#     context={
-#         'post' : post,
-#         'form' : form,
-#         "ratings": ratings,
-#     }
-#     return render(request, 'AwardsHome/rating.html', context)
+    ratings = Ratings.objects.filter(post=post).order_by('-created_on')
+    context={
+        'post' : post,
+        'form' : form,
+        "ratings": ratings,
+    }
+    return render(request, 'AwardsHome/rating.html', context)
