8367eacd55d0910235c0538237199f5f7c5fdd93
Irene-nandy <irenekasiva75@gmail.com>
2020-10-25 18:58:43 +0300

    add admin site

--- AwardsPosts/admin.py
+++ AwardsPosts/admin.py
@@ -1,7 +1,7 @@
-# from django.contrib import admin
-# from .models import Post, Ratings
+from django.contrib import admin
+from .models import Post, Ratings
 
-# # Register your models here.
-# admin.site.register(Post)
-# admin.site.register(Ratings)
+# Register your models here.
+admin.site.register(Post)
+admin.site.register(Ratings)
 
